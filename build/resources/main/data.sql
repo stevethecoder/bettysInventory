9afded6f12904aa51587f4dc01ae6e09e241b4f7
Steven Richards <steve@littlecreekdesign.com>
2016-11-12 14:32:48 -0500

    initial commit

--- build/resources/main/data.sql
+++ build/resources/main/data.sql
@@ -1,1 +1,24 @@
-insert into robotsanddonuts.products (upc, product_name, product_manufacturer, product_price) values ('d98f8c8gg', 'JellyRumblers', 'Titus', '9.99');
+insert into bettysinventory.products (product_id, product_name, product_manufacturer, product_price) values ('10001', 'Olive Sheath Dress', 'ModCloth', 59.99);
+insert into bettysinventory.products (product_id, product_name, product_manufacturer, product_price) values ('10002', 'Joys A-Line Dress', 'ModCloth', 99.99);
+insert into bettysinventory.products (product_id, product_name, product_manufacturer, product_price) values ('10003', 'Ivory Tank Top', 'ModCloth', 39.99);
+insert into bettysinventory.products (product_id, product_name, product_manufacturer, product_price) values ('10004', 'Photoshoot Sweater', 'ModCloth', 49.99);
+insert into bettysinventory.products (product_id, product_name, product_manufacturer, product_price) values ('10005', 'Jeans Overalls', 'ModCloth', 79.99);
+insert into bettysinventory.products (product_id, product_name, product_manufacturer, product_price) values ('10006', 'Knit Tunic', 'J Jill', 29.99);
+insert into bettysinventory.products (product_id, product_name, product_manufacturer, product_price) values ('10013', 'Dream Dress', 'Historical Clothing', 62.99);
+
+insert into bettysinventory.customers (first_name, last_name, email, customer_since) values ('Steven', 'Richards', 'steve@littlecreekdesign.com', 1979);
+insert into bettysinventory.customers (first_name, last_name, email, customer_since) values ('Lefty', 'Frizzell', 'lefty@yourdime.com', 1974);
+insert into bettysinventory.customers (first_name, last_name, email, customer_since) values ('Tader', 'Tott', 'tader@youvegotthetader.com', 1989);
+insert into bettysinventory.customers (first_name, last_name, email, customer_since) values ('Albert', 'Einstein', 'emc2@relativity.com', 1956);
+insert into bettysinventory.customers (first_name, last_name, email, customer_since) values ('Pablo', 'Picasso', 'abstractart@impressions.com', 1961);
+
+insert into bettysinventory.transactions (customer_id, product_id, transaction_date) values (1, 1, 'January 30, 2016');
+insert into bettysinventory.transactions (customer_id, product_id, transaction_date) values (1, 4, 'January 30, 2016');
+insert into bettysinventory.transactions (customer_id, product_id, transaction_date) values (1, 6, 'January 30, 2016');
+insert into bettysinventory.transactions (customer_id, product_id, transaction_date) values (2, 1, 'March 3, 2016');
+insert into bettysinventory.transactions (customer_id, product_id, transaction_date) values (3, 1, 'May 6, 2016');
+insert into bettysinventory.transactions (customer_id, product_id, transaction_date) values (4, 1, 'November 8, 2016');
+insert into bettysinventory.transactions (customer_id, product_id, transaction_date) values (4, 2, 'November 8, 2016');
+insert into bettysinventory.transactions (customer_id, product_id, transaction_date) values (4, 3, 'November 8, 2016');
+insert into bettysinventory.transactions (customer_id, product_id, transaction_date) values (4, 4, 'November 8, 2016');
+insert into bettysinventory.transactions (customer_id, product_id, transaction_date) values (5, 1, 'August 14, 2016');
--- build/resources/main/schema.sql
+++ build/resources/main/schema.sql
@@ -1,10 +1,32 @@
-CREATE SCHEMA IF NOT EXISTS robotsanddonuts;
-USE robotsanddonuts;
+CREATE SCHEMA IF NOT EXISTS bettysinventory;
+USE bettysinventory;
 
-CREATE TABLE IF NOT EXISTS robotsanddonuts.products (
+CREATE TABLE IF NOT EXISTS bettysinventory.products (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   product_id VARCHAR(16) NOT NULL,
-  product_name VARCHAR(16) NOT NULL,
-  product_manufacturer VARCHAR(45) NOT NULL,
-  product_price DECIMAL NOT NULL,
+  product_name VARCHAR(36) NOT NULL,
+  product_manufacturer VARCHAR(36) NOT NULL,
+  product_price VARCHAR(36) NOT NULL,
+  PRIMARY KEY (id));
+  
+CREATE TABLE IF NOT EXISTS bettysinventory.product_images (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  product_id INT UNSIGNED NOT NULL,
+  content_type VARCHAR(45) NOT NULL,
+  image BLOB NOT NULL,
+  PRIMARY KEY (id));
+  
+CREATE TABLE IF NOT EXISTS bettysinventory.customers (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(16) NOT NULL,
+  last_name VARCHAR(36) NOT NULL,
+  email VARCHAR(36) NOT NULL,
+  customer_since VARCHAR(36) NOT NULL,
+  PRIMARY KEY (id));
+  
+CREATE TABLE IF NOT EXISTS bettysinventory.transactions (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  customer_id VARCHAR(16) NOT NULL,
+  product_id VARCHAR(16) NOT NULL,
+  transaction_date VARCHAR(40) NOT NULL,
   PRIMARY KEY (id));
