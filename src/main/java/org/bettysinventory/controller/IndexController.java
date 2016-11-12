7bdce74d0e9a79754e68031bf1612ca23396df11
Steven Richards <steve@littlecreekdesign.com>
2016-11-12 15:37:15 -0500

    initial commit

--- src/main/java/org/bettysinventory/controller/IndexController.java
+++ src/main/java/org/bettysinventory/controller/IndexController.java
@@ -1,41 +1,37 @@
 package org.bettysinventory.controller;
 
-import javax.validation.Valid;
-
-import org.bettysinventory.beans.Product;
-import org.bettysinventory.beans.ProductImage;
-import org.bettysinventory.repository.ProductImageRepository;
-import org.bettysinventory.repository.ProductRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class IndexController {
-		
-	@GetMapping("")
+
+//	@Autowired
+//	private ProductImageRepository productImageRepo;
+	
+	@GetMapping("/products")
 	public String index(Model model) {
-		return "index";
+		return "products";
 	}
 
-	@GetMapping("/home")
-	public String home(Model model){
-		return "home";
+//	@GetMapping("/customers")
+//	public String home(Model model) {
+//		return "customers";
+//	}
+	
+//	@GetMapping("/login")
+//	public String login(Model model) {
+//		return "login";
+//	}
+
+
+
+
 	}
-	@GetMapping("/login")
-	public String login(Model model) {
-		return "login";
-	}
+		
+
+	
 	
 
-}
+
