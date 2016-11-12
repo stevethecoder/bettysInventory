e0670c04aafff351973671acadb4f9625d8f5075
Steven Richards <steve@littlecreekdesign.com>
2016-11-12 14:05:58 -0500

    111216 for portfolio

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,7 +4,6 @@ insert into bettysinventory.products (product_id, product_name, product_manufact
 insert into bettysinventory.products (product_id, product_name, product_manufacturer, product_price) values ('10004', 'Photoshoot Sweater', 'ModCloth', 49.99);
 insert into bettysinventory.products (product_id, product_name, product_manufacturer, product_price) values ('10005', 'Jeans Overalls', 'ModCloth', 79.99);
 insert into bettysinventory.products (product_id, product_name, product_manufacturer, product_price) values ('10006', 'Knit Tunic', 'J Jill', 29.99);
-insert into bettysinventory.products (product_id, product_name, product_manufacturer, product_price) values ('10007', 'Boatneck Tee', 'Ann Taylor', 24.99);
 insert into bettysinventory.products (product_id, product_name, product_manufacturer, product_price) values ('10013', 'Dream Dress', 'Historical Clothing', 62.99);
 
 insert into bettysinventory.customers (first_name, last_name, email, customer_since) values ('Steven', 'Richards', 'steve@littlecreekdesign.com', 1979);
