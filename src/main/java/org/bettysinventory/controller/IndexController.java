ab57c546f35710bb702b78f3ea04791f4fc72333
Steven Richards <steve@littlecreekdesign.com>
2016-11-12 15:58:15 -0500

    second commit

--- src/main/java/org/bettysinventory/controller/IndexController.java
+++ src/main/java/org/bettysinventory/controller/IndexController.java
@@ -1,37 +1,33 @@
 package org.bettysinventory.controller;
 
+import javax.validation.Valid;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class IndexController {
-
-//	@Autowired
-//	private ProductImageRepository productImageRepo;
-	
-	@GetMapping("/products")
-	public String index(Model model) {
-		return "products";
-	}
-
-//	@GetMapping("/customers")
-//	public String home(Model model) {
-//		return "customers";
-//	}
-	
-//	@GetMapping("/login")
-//	public String login(Model model) {
-//		return "login";
-//	}
-
-
-
-
-	}
 		
+	@GetMapping("")
+	public String index(Model model) {
+		return "index";
+	}
 
+	@GetMapping("/home")
+	public String home(Model model){
+		return "home";
+	}
+	@GetMapping("/login")
+	public String login(Model model) {
+		return "login";
+	}
 	
-	
 
-
+}
